626d09b311e969e5dac30a552c11b5c2852fa9e5
mouaadhalimi <halimimouaad20@gmail.com>
2025-03-20 21:12:10 +0100

    end of the 03/20/2025

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ __version__ = "0.0.0"
 
 REPO_NAME ="Chicken-Disease-Classification"
 AUTHOR_USER_NAME = "mouaadhalimi"
-SRC_REPO ="Chicken-Disease-Classification"
+SRC_REPO ="cnnClassifier"
 AUTHOR_EMAIL= "halimimouaad20@gmail.com"
 
 
